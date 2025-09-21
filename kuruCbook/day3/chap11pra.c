c4abe616ec0a2acfd5b53ea7f4123bec8f180840
RyNtake <ry.yoshirin@gmail.com>
2025-09-21 16:33:21 +0900

    chap11pra OK

---
+++ kuruCbook/day3/chap11pra.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int olympicYearJudge(int);
+
+int main(void)
+{
+  int year;
+  do
+  {
+    printf("2000年以降の西暦を入力してください。：");
+    scanf("%d",&year);
+  } while (year < 2000);
+
+  switch (olympicYearJudge(year))
+  {
+  case 0:
+    printf("その年は夏季オリンピックの開催年です。\n");
+    break;
+
+  case 2:
+    printf("その年は冬季オリンピックの開催年です。\n");
+    break;
+
+  default:
+    printf("その年にオリンピックは開かれていません。\n");
+    break;
+  }
+
+  return 0;
+}
+
+int olympicYearJudge(int year)
+{
+  int whitchSeason = year % 4;
+  return whitchSeason;
+}
