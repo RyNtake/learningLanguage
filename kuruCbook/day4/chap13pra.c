743c0775a7a7fb95dc7eae3cf0b051ab29e530fe
RyNtake <ry.yoshirin@gmail.com>
2025-09-22 17:17:28 +0900

    chap13pra 入力センサは未完成

--- kuruCbook/day4/chap13pra.c
+++ kuruCbook/day4/chap13pra.c
@@ -2,6 +2,25 @@
 
 int main(void)
 {
+  /* 入力された10個の数字を最後から表示するプログラム*/
+  int array[10];
+  int i = 0;
+
+  for (int i = 0; i <= 10; i++)
+  {
+    do
+    {
+      printf("%2dつ目の整数を入力してください。:", i + 1);
+      scanf("%d", &array[i]);
+    } while (array[i] % 1 != 0);
+  }
+
+  for (int i = 10-1; i >= 0; i--)
+  {
+    printf("%d ", array[i]);
+  }
+
+  printf("\n");
 
   return 0;
 }
